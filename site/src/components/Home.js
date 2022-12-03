91dac89e3de74b6645566890bb4b40af48d9879f
syzhao53 <syzhao@sas.upenn.edu>
2022-12-02 23:48:11 -0500

    form submit setup

--- site/src/components/Home.js
+++ site/src/components/Home.js
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react/jsx-filename-extension */
 /* eslint-disable */
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { HashLink as Link } from 'react-router-hash-link';
 import { useNavigate } from 'react-router-dom';
 import '../assets/index.css';
@@ -34,7 +34,49 @@ function Home({ currPage, setCurrPage }) {
   //   }
   // };
 
+  const [error, setError] = useState({ message: '' });
+  const [one, setOne] = useState('');
+  const [two, setTwo] = useState('');
+  const [three, setThree] = useState('');
+  const [four, setFour] = useState('');
+  const [five, setFive] = useState('');
+  const [six, setSix] = useState('');
+
+
   const footerMessage = 'Made by Sylvia';
+
+  const convert = (s) => {
+    s = s.trim();
+
+    if (s == 'j' || s == 'J') {
+      return 11;
+    } else if (s == 'q' || s == 'Q') {
+      return 12;
+    } else if (s == 'k' || s == 'K') {
+      return 13;
+    } else if (s == 'a' || s == 'A') {
+      return 1;
+    } else {
+      return parseInt(s);
+    }
+  }
+  const solveForm = (event) => {
+    // log.textContent = 'Form Submitted! Time stamp: ${event.timeStamp}';
+    event.preventDefault();
+    const oneInt = convert(one)
+    const twoInt = convert(two);
+    const threeInt = convert(three);
+    const fourInt = convert(four);
+    const fiveInt = convert(five);
+    const sixInt = convert(six);
+
+    const total = oneInt + twoInt;
+    setError({ message: 'Please enter your login credentials' + total });
+  }
+  
+  // const form = document.getElementById('solver-form');
+  // const log = document.getElementById('log');
+  // form.addEventListener('submit', solveForm);
 
   useEffect(() => {
     setCurrPage('work');
@@ -53,7 +95,7 @@ function Home({ currPage, setCurrPage }) {
         <div className="input-flex">
         <img id="illus" src={illustration} alt="landing illustration" />
 
-          <form className="input-form">
+          <form className="input-form" id="solver-form"  onSubmit={solveForm}>
             <div className="big-header">
               163 SOLVER
             </div>
@@ -61,15 +103,16 @@ function Home({ currPage, setCurrPage }) {
               Input 1-13 or J, Q, K, A
             </div>
             <div className="input-row">
-              <input className="input-in" type="text"></input>
-              <input className="input-in" type="text"></input>
-              <input className="input-r" type="text"></input>
+              <input className="input-in" type="text" onChange={event => setOne(event.target.value)}></input>
+              <input className="input-in" type="text" onChange={event => setTwo(event.target.value)}></input>
+              <input className="input-r" type="text" onChange={event => setThree(event.target.value)}></input>
             </div>
             <div className="input-row-b">
-              <input className="input-in" type="text"></input>
-              <input className="input-in" type="text"></input>
-              <input className="input-r" type="text"></input>
+              <input className="input-in" type="text" onChange={event => setFour(event.target.value)}></input>
+              <input className="input-in" type="text" onChange={event => setFive(event.target.value)}></input>
+              <input className="input-r" type="text" onChange={event => setSix(event.target.value)}></input>
             </div>
+            <div id="error">{error.message}</div>
             <div className="submit-btn">
               <input type="submit" id="download" value="SOLVE"></input>
             </div>
